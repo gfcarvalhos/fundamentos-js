f0d15e90fa94b198ad0ef05796fe5a65961dd99c
Silva Carvalho Gabriel Felipe Carvalho <n134324@gruposantander.com>
2023-05-23 22:29:19 -0300

    Tipos primitos JS

---
+++ media.js
@@ -0,0 +1,25 @@
+
+//Tipo number
+const nota1 = 10;
+const nota2 = 6.5;
+const nota3 = 8;
+const nota4 = 7.5;
+
+console.log("É um texto para teste não é?");
+
+console.log(nota1*nota3)
+
+//Tipo String
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); //False
+
+console.log(cidade === input.toLowerCase()); //True
+
+console.log(cidade.length);
+
+//Tipo booleano
+let cadastroAtivo = true;
+
+console.log(cadastroAtivo);
